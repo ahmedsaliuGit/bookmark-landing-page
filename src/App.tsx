98c20bfc93a645d78b58173e7cc62165dda4592d
ahmedsaliuGit <saliu.lito@gmail.com>
2023-05-11 09:40:07 +0100

    Bookmark landing page init commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-center text-2xl mt-8">Setting up App</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+};
