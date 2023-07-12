7f44c09949d5b2d24f2ad981b8f5d4f21000987c
ahmedsaliuGit <saliu.lito@gmail.com>
2023-07-12 17:12:47 +0100

    Added accordion and mobile nav

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,48 @@
+type AccordionProps = {
+  title: string;
+  body: string;
+  selected: string;
+  setAccordion: (accordion: string) => void;
+};
+
+export const Accordion = function ({
+  title,
+  body,
+  selected,
+  setAccordion,
+}: AccordionProps) {
+  const handleClick = (accordion: string) => {
+    if (accordion === selected) {
+      setAccordion("");
+    } else {
+      setAccordion(title);
+    }
+  };
+
+  return (
+    <div className="w-full flex flex-col">
+      <button
+        className="flex justify-between py-4 items-center border-b-[1px]"
+        onClick={() => handleClick(title)}
+      >
+        <span className="font-normal">{title}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="12"
+          className={`${selected === title ? "rotate-180" : ""}`}
+        >
+          <path
+            fill="none"
+            stroke={`${selected === title ? "hsl(0, 94%, 66%)" : "#5267DF"}`}
+            strokeWidth="3"
+            d="M1 1l8 8 8-8"
+          />
+        </svg>
+      </button>
+      {selected === title && (
+        <p className="my-5 text-grayish-blue text-left">{body}</p>
+      )}
+    </div>
+  );
+};
--- src/components/FrequentSection.tsx
+++ src/components/FrequentSection.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import { Accordion } from "./Accordion";
 import Section from "./Section";
 
 export default function FrequentSection() {
+  const [active, setActive] = useState("");
+
+  const onClick = (accordion: string) => {
+    setActive(accordion);
+  };
+
   return (
     <Section>
       <div className="w-full px-10 text-center mt-28">
@@ -9,7 +17,39 @@ export default function FrequentSection() {
           Here are some of our FAQs. If you have any other questions you’d like
           answered please feel free to email us.
         </p>
-        <div>Accordion Area</div>
+        <div className="w-full my-8">
+          <Accordion
+            title="What is Bookmark?"
+            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt 
+  justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis."
+            selected={active}
+            setAccordion={onClick}
+          />
+          <Accordion
+            title="How can I request a new browser?"
+            body="Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. 
+  Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, 
+  ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. 
+  Mauris augue massa, ultricies non ligula. Suspendisse imperdiet."
+            selected={active}
+            setAccordion={onClick}
+          />
+          <Accordion
+            title="Is there a mobile app?"
+            body="Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum 
+  urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed 
+  sollicitudin ex et ultricies bibendum."
+            selected={active}
+            setAccordion={onClick}
+          />
+          <Accordion
+            title="What about other Chromium browsers?"
+            body="Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam 
+  vitae neque eget nisl gravida pellentesque non ut velit."
+            selected={active}
+            setAccordion={onClick}
+          />
+        </div>
         <a
           href="#"
           className="bg-soft-blue p-4 font-normal text-white rounded-lg inline-block my-4"
